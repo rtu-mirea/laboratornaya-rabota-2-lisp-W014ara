044cd9ccc20ee1c5a6aaf45890546a14666a9c98
W014ara <bezruk2000@mail.ru>
2019-11-02 11:33:07 +0300

    ver2

--- lab2.lisp
+++ lab2.lisp
@@ -20,7 +20,7 @@
 
 ;; Задание 2
 (defun out (path)
-(let ((in (open "test.txt" :if-does-not-exist nil)))
+(let ((in (open path :if-does-not-exist nil)))
 (when in
 (loop for line = (read-line in nil)
 while line do (format t "~a~%" line))
